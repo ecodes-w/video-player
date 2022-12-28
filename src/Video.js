1e5ba8f07186369481a2818c848d3bb62c65a8fa
PANTOJA <ericapantoja@northwesternmutual.com>
2022-12-28 16:38:30 -0600

    creating the components of the app

---
+++ src/Video.js
@@ -0,0 +1,10 @@
+import React from "react";
+export class Video extends React.Component {
+  render() {
+    return (
+      <div>
+        <video controls autostart autoplay muted src={this.props.src}></video>
+      </div>
+    );
+  }
+}
